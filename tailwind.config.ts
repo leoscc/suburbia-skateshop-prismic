1cdcf7ccebe88f72ceb1cc179bcecf42360f18b6
leoscc <leonardoscc15@gmail.com>
2025-02-22 16:48:37 -0300

    feat: add project colors.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,8 +14,13 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        "brand-blue": "#4876ff",
+        "brand-lime": "#d9f154",
+        "brand-navy": "#2e3192",
+        "brand-orange": "#ff7347",
+        "brand-pink": "#f7d0e9",
+        "brand-purple": "#692e54",
+        "brand-gray": "#fffdf9",
       },
       fontFamily: {
         sans: ["var(--font-bowlby-sc)"],
