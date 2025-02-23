c87a5f3a56beec42444365f32e78d78c6ffa5fbe
leoscc <leonardoscc15@gmail.com>
2025-02-23 14:35:42 -0300

    feat: position header elements.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,19 +5,24 @@ type Props = {};
 
 export default function Header({}: Props) {
   return (
-    <header className="header absolute inset-0 z-50 ~h-32/48 ~p-4/6 md:h-32">
-      <div>
-        <Link href="/">Logo</Link>
+    <header className="header absolute top-0 left-0 right-0 z-50 ~h-32/48 ~p-4/6 md:h-32">
+      <div className="mx-auto w-full max-w-6xl grid grid-cols-[auto,auto] md:grid-cols-[1fr,auto,1fr] gap-6 items-center">
+        <Link href="/" className="justify-self-start">
+          Logo
+        </Link>
 
-        <nav>
-          <ul>
+        <nav
+          aria-label="Main"
+          className="col-span-full row-start-2 md:col-span-1 md:col-start-2 md:row-start-1"
+        >
+          <ul className="flex flex-wrap items-center justify-center gap-8">
             <li>Boards</li>
             <li>Boards</li>
             <li>Boards</li>
           </ul>
         </nav>
 
-        <div>
+        <div className="justify-self-end">
           <ButtonLink href="" icon="cart" color="purple" aria-label="Cart (1)">
             Cart (1)
           </ButtonLink>
