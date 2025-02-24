18e70ab311bccb527643196bcd6a491774e2fa24
leoscc <leonardoscc15@gmail.com>
2025-02-24 19:30:00 -0300

    feat: adapt cart button text on mobile.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -24,7 +24,8 @@ export default function Header({}: Props) {
 
         <div className="justify-self-end">
           <ButtonLink href="" icon="cart" color="purple" aria-label="Cart (1)">
-            Cart (1)
+            <span className="md:hidden">1</span>
+            <span className="hidden md:inline">Cart (1)</span>
           </ButtonLink>
         </div>
       </div>
